2f2ed89948b9378df0766660d11da9ea6aec8baf
cuilitang <cuilitang@126.com>
2015-05-28 18:58:33 +0800

    2015-5-28 18:58:30

--- src/cui/litang/cuiweather/app/receiver/AutoupdateRecevier.java
+++ src/cui/litang/cuiweather/app/receiver/AutoupdateRecevier.java
@@ -5,6 +5,11 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+/**
+ * service与receiver互调，保证service的运行。
+ * @author Cuilitang
+ * @Date 2015年5月28日
+ */
 public class AutoupdateRecevier extends BroadcastReceiver {
 
 	@Override
